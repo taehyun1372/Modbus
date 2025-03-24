c367aa9d1479c35394f303c3f3ec6834747ba6ec
DotNetTutorial <85148810+roy1117@users.noreply.github.com>
2025-03-25 00:40:25 +0900

    Add Pop up Window

---
+++ Practice/6_Popup_Window/6_Popup_Window/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _6_Popup_Window.Views;
+
+namespace _6_Popup_Window
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PopUpWindowView _popup;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _popup = new PopUpWindowView();
+            _popup.Show();
+        }
+    }
+}
