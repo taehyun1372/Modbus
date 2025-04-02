fc702a11a81cec2e7f8b2106c522ab67432d5415
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-04-02 18:54:46 +0900

    Added Data Grid Dynamic Ordering Soultion

---
+++ Practice/10_DataGrid_Dynamic_Ordering/10_DataGrid_Dynamic_Ordering/MainDataGridView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10_DataGrid_Dynamic_Ordering
+{
+    /// <summary>
+    /// Interaction logic for MainDataGridView.xaml
+    /// </summary>
+    public partial class MainDataGridView : UserControl
+    {
+        public MainDataGridView(MainDataGridViewModel model)
+        {
+            InitializeComponent();
+            this.DataContext = model;
+        }
+    }
+}
---
+++ Practice/10_DataGrid_Dynamic_Ordering/10_DataGrid_Dynamic_Ordering/MainDataGridViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace _10_DataGrid_Dynamic_Ordering
+{
+    public class MainDataGridViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Dictionary<string, string>> _dataItems = new ObservableCollection<Dictionary<string, string>>();
+        public ObservableCollection<Dictionary<string, string>> DataItems
+        {
+            get
+            {
+                return _dataItems;
+            }
+            set
+            {
+                _dataItems = value;
+                OnPropertyChanged(nameof(DataItems));
+            }
+        }
+
+        public MainDataGridViewModel()
+        {
+            var dic1 = new Dictionary<string, string> { {"Name", "Alice" }, { "Age", "25"} };
+
+            var dic2 = new Dictionary<string, string> { { "Name", "Bob" }, { "Age", "30" } };
+
+            var dic3 = new Dictionary<string, string> { { "Name", "Charlie" }, { "Age", "35" } };
+
+            DataItems.Add(dic1);
+            DataItems.Add(dic2);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DataItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private int _value;
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                OnPropertyChanged(nameof(Value));
+            }
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
