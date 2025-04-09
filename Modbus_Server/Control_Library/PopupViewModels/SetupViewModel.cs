6cee1130ce1f5d1f759f3cbd3e3a99b929902264
DotNetTutorial <85148810+roy1117@users.noreply.github.com>
2025-04-10 05:32:33 +0900

    Created setup popup
    add

---
+++ Modbus_Server/Control_Library/PopupViewModels/SetupViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+
+namespace Control_Library.PopupViewModels
+{
+    public class SetupViewModel : INotifyPropertyChanged
+    {
+        private int _row;
+        public int Row
+        {
+            get { return _row; }
+            set
+            {
+                _row = value;
+                OnPropertyChanged(nameof(Row));
+            }
+        }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _quantity = value;
+                OnPropertyChanged(nameof(Quantity));
+            }
+        }
+
+        public event Action<object, SetupFinishedEventArg> SetupFinished;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SetupViewModel()
+        {
+            Row = 10;
+            Quantity = 10;
+        }
+
+        public void OkayClickedHandler(object sender, RoutedEventArgs e)
+        {
+            SetupFinished?.Invoke(this, new SetupFinishedEventArg(Row, Quantity));
+        }
+
+        public void CancelClickedHandler(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class SetupFinishedEventArg
+    {
+        public int Row { get; set; }
+        public int Quantity { get; set; }
+
+        public SetupFinishedEventArg(int row, int quantity)
+        {
+            Row = row;
+            Quantity = quantity;
+        }
+    }
+        
+
+}
---
+++ Modbus_Server/Test_Bey/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Control_Library.PopupViewModels;
+using Control_Library.PopupViews;
+
+namespace Test_Bey
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SetupViewModel _setupViewModel;
+        private SetupView _setupView;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            _setupViewModel = new SetupViewModel();
+            _setupView = new SetupView(_setupViewModel);
+
+            _setupViewModel.SetupFinished += UpdateTextBlocks;
+
+            _setupView.ShowDialog();
+        }
+
+        private void UpdateTextBlocks(object sender, SetupFinishedEventArg e)
+        {
+            tbRow.Text = e.Row.ToString();
+            tbQuantity.Text = e.Quantity.ToString();
+        }
+    }
+}
