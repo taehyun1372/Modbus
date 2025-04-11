e4da21f4d0d2c928f927c831d9822093f9813e0e
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-04-11 15:12:06 +0900

    Added serialization/Deserialization solution

---
+++ Practice/12_Serialization_Deserialization/12_Serialization_Deserialization/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace _12_Serialization_Deserialization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SetupViewModel _model;
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupViewModel model = new SetupViewModel();
+            SetupView view = new SetupView(model);
+            _model = model;
+            fmMain.Content = view;
+        }
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SetupViewModel));
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\a00533064\OneDrive - ONEVIRTUALOFFICE\Desktop\SerializationTest\Test123.xml"))
+            {
+                serializer.Serialize(writer, _model);
+            }
+            MessageBox.Show("Object Serialized Successfully");
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs erg)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SetupViewModel));
+            SetupViewModel tempModel = new SetupViewModel();
+            try
+            {
+                using (StreamReader reader = new StreamReader(@"C:\Users\a00533064\OneDrive - ONEVIRTUALOFFICE\Desktop\SerializationTest\Test123.xml"))
+                {
+                    tempModel = (SetupViewModel)serializer.Deserialize(reader);
+                }
+
+                MessageBox.Show("Object Deserialized Successfully");
+
+                _model.FunctionType = tempModel.FunctionType;
+                _model.RowSetting = tempModel.RowSetting;
+                _model.QuantitySetting = tempModel.QuantitySetting;
+                _model.PLCAddress = tempModel.PLCAddress;
+                _model.AddressVisible = tempModel.AddressVisible;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Object Deserialized Failed \n {e.Message}");
+            }
+        }
+    }
+}
---
+++ Practice/12_Serialization_Deserialization/12_Serialization_Deserialization/SetupViewModel.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+
+namespace _12_Serialization_Deserialization
+{
+    public class SetupViewModel : INotifyPropertyChanged
+    {
+        public enum FunctionTypes
+        {
+            coil = 0,
+            status = 1,
+            inputRegister = 2,
+            holdingRegister = 3
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private FunctionTypes _functontype;
+        public FunctionTypes FunctionType
+        {
+            get
+            {
+                return _functontype;
+            }
+            set
+            {
+                _functontype = value;
+                if (value == FunctionTypes.coil)
+                {
+                    Coil = true;
+                    Status = false;
+                    InputRegister = false;
+                    HoldingRegister = false;
+                }
+                else if(value == FunctionTypes.status)
+                {
+                    Coil = false;
+                    Status = true;
+                    InputRegister = false;
+                    HoldingRegister = false;
+                }
+                else if (value == FunctionTypes.inputRegister)
+                {
+                    Coil = false;
+                    Status = false;
+                    InputRegister = true;
+                    HoldingRegister = false;
+                }
+                else if (value == FunctionTypes.holdingRegister)
+                {
+                    Coil = false;
+                    Status = false;
+                    InputRegister = false;
+                    HoldingRegister = true;
+                }
+            }
+        }
+
+        private int _rowSetting;
+        public int RowSetting
+        {
+            get
+            {
+                return _rowSetting;
+            }
+            set
+            {
+                _rowSetting = value;
+                OnPropertyChanged(nameof(RowSetting));
+            }
+        }
+
+        private int _quantitySetting;
+        public int QuantitySetting
+        {
+            get
+            {
+                return _quantitySetting;
+            }
+            set
+            {
+                _quantitySetting = value;
+                OnPropertyChanged(nameof(QuantitySetting));
+            }
+        }
+
+        private bool _coil;
+        public bool Coil
+        {
+            get
+            {
+                return _coil;
+            }
+            set
+            {
+                _coil = value;
+                if (value)
+                {
+                    _functontype = FunctionTypes.coil;
+                }
+                
+                OnPropertyChanged(nameof(Coil));
+            }
+        }
+
+        private bool _status;
+        public bool Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+                if (value)
+                {
+                    _functontype = FunctionTypes.status;
+                }
+                    
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        private bool _inputRegister;
+        public bool InputRegister
+        {
+            get
+            {
+                return _inputRegister;
+            }
+            set
+            {
+                _inputRegister = value;
+                if (value)
+                {
+                    _functontype = FunctionTypes.inputRegister;
+                }
+                
+                OnPropertyChanged(nameof(InputRegister));
+            }
+        }
+
+        private bool _holidngRegister;
+        public bool HoldingRegister
+        {
+            get
+            {
+                return _holidngRegister;
+            }
+            set
+            {
+                _holidngRegister = value;
+                if (value)
+                {
+                    _functontype = FunctionTypes.holdingRegister;
+                }
+                
+                OnPropertyChanged(nameof(HoldingRegister));
+            }
+        }
+
+        private bool _pLCAddress;
+        public bool PLCAddress
+        {
+            get
+            {
+                return _pLCAddress;
+            }
+            set
+            {
+                _pLCAddress = value;
+                OnPropertyChanged(nameof(PLCAddress));
+            }
+        }
+
+        private bool _addressVisible;
+        public bool AddressVisible
+        {
+            get
+            {
+                return _addressVisible;
+            }
+            set
+            {
+                _addressVisible = value;
+                OnPropertyChanged(nameof(AddressVisible));
+            }
+        }
+
+        public SetupViewModel()
+        {
+            RowSetting = 10;
+            QuantitySetting = 5;
+            FunctionType = FunctionTypes.holdingRegister;
+            PLCAddress = true;
+            AddressVisible = false;
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
