8db66882c429749e2b02860aba15f04248961275
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-05-14 18:23:36 +0900

    [27_Dispose]

---
+++ Practice/27_Dispose/27_Dispose/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _27_Dispose
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Popup _popup;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPopup_Click(object sender, RoutedEventArgs e)
+        {
+            if(_popup == null)
+            {
+                _popup = new Popup();
+                _popup.Closed += (s, arg) => {
+                    _popup.Dispose();
+                    _popup = null;
+                };
+                _popup.Show();
+            }
+            else
+            {
+                _popup.Activate();
+            }
+        }
+    }
+}
