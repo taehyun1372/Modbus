290d92508e32c7bb48c51107afc48d23b67c6b62
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-05-20 15:33:29 +0900

    [29_Relay_Command]

---
+++ Practice/29_Relay_Command/29_Relay_Command/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.ComponentModel;
+
+namespace _29_Relay_Command
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _value;
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                if (_test1Command != null)
+                {
+                    var relayComamnd = (RelayCommand)_test1Command;
+                    relayComamnd.CheckExecute();
+                }
+                OnPropertyChanged(nameof(Value));
+            }
+        }
+
+        private ICommand _test1Command;
+
+        public ICommand Test1Command
+        {
+            get
+            {
+                if (_test1Command == null)
+                {
+                    _test1Command = new RelayCommand(_ => MessageBox.Show($"{this.Value}"), _ => this.Value > 100);
+                }
+                return _test1Command;
+            }
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public class MyICommand : ICommand
+        {
+            public event EventHandler CanExecuteChanged;
+            private MainViewModel _model;
+
+            public MyICommand(MainViewModel model)
+            {
+                _model = model;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return (_model.Value == 100);
+            }
+
+            public void Execute(object parameter)
+            {
+                MessageBox.Show("Something");
+            }
+
+            public void RaiseCanExecuteChanged()
+            {
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Practice/29_Relay_Command/29_Relay_Command/Utilities.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _29_Relay_Command
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+            {
+                return _canExecute(parameter);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void CheckExecute()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
